748a7099cd9ea4dd25c91c6023970e7dbf2ac7b4
methun12 <69069999+methun12@users.noreply.github.com>
2022-05-13 16:16:30 +0600

    Update firebaseLibByMethun.js

--- firebaseLibByMethun.js
+++ firebaseLibByMethun.js
@@ -2,14 +2,17 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.0/firebase
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.8.0/firebase-analytics.js";
 import {writex} from "./set.js"
 import {readx} from "./get.js"
-export function _initialize(config_obj){
-      const app = initializeApp(config_obj);
-      const analytics = getAnalytics(app);     
+export let firebaseLib=function(){
+  this.initialize=(config_obj)=>{
+    const app = initializeApp(config_obj);
+    const analytics = getAnalytics(app);
+  };
+  this.write=(referenceName, dataObject)=>{
+    return writex(referenceName, dataObject);
+  }
+  this.read=(referenceNameR)=>{
+    return readx(referenceNameR);
+  }
 }
-export function write(referenceName,dataObject){
-  return writex(referenceName,dataObject);
-}
-export function read(referenceNameR){
-  return readx(referenceNameR);
-}
+
 //export{write,read};
